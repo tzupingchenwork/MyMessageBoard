2e53e7e4540be3a6be8419cba646b7fbe599ca03
Andy <>
2019-07-31 14:48:24 +0800

    完成功能 ng-container 不能一直自動產生 所以使用 ul li

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Message } from './message';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'MyMessageBoard';
+  name = '';
+  content = '';
+  messages: Message[] = [];
+  addMessage(): void {
+    // 防呆，避免名稱或內容是空值時也可以留言
+     if (
+       !this.name.trim() || !this.content.trim()
+     ) {
+       return;
+     }
+     // 用名稱跟內容產生一個留言的資料物件
+     const message = new Message(this.name, this.content);
+     // 將留言的資料物件放進容器裡
+     this.messages.push(message);
+     // 清空內容
+     this.content = '';
+   }
 }
+
